5f8455223401b7140c6e2c57ac272be1ae10635c
aurelien.izoulet <aurelien.izoulet@epita.fr>
2023-12-13 13:03:35 +0100

    update to pow 2

--- malloc_v1/src/tools.c
+++ malloc_v1/src/tools.c
@@ -1,8 +1,8 @@
 #include "tools.h"
 
-size_t size_align(size_t size)
+size_t size_align_(size_t size)
 {
-
+    // Align to 16 bytes
     const size_t align_base = sizeof(long double);
     const size_t mask = align_base - 1;
 
@@ -12,6 +12,27 @@ size_t size_align(size_t size)
     }
 
     return (size + mask) & ~mask;
+}
+
+size_t size_align(size_t size)
+{
+    if (size < 16)
+    {
+        return 16;
+    }
+    // Align to the closed power of two
+    if (size > (size_t)(-1) / 2 + 1)
+    {
+        return 0;
+    }
+
+    size_t mask = 1;
+    while (mask < size)
+    {
+        mask <<= 1;
+    }
+
+    return mask;
 }
 
 void *page_begin(void *ptr, size_t page_size)
