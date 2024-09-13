1c2850eb49b243ed3c18b6b8a219ec68e9dd80f6
Izoulet Aurélien <aurelien.izoulet@epita.fr>
2024-09-13 15:32:01 +0200

    minor change

--- malloc_v1/tests/malloc.c
+++ malloc_v1/tests/malloc.c
@@ -268,7 +268,7 @@ Test(my_malloc, mixed_random_operations)
     clock_t start_time = clock();
 
     void *ptr = NULL;
-    const int iterations = 100000;
+    const int iterations = 300000;
     for (int i = 0; i < iterations; i++) {
         switch (rand() % 4) {
             case 0: // malloc
